2f65c29788774c6c0250ffdce256cd47216435f7
Benjamin Mahr <ben.mahr@gmail.com>
2015-03-03 21:49:14 +0100

    zeiten

--- app/src/main/java/robinben/hsr/ch/aesboeboe/ConnectionAdapter.java
+++ app/src/main/java/robinben/hsr/ch/aesboeboe/ConnectionAdapter.java
@@ -7,7 +7,12 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import ch.schoeb.opendatatransport.model.Connection;
 import ch.schoeb.opendatatransport.model.ConnectionList;
@@ -50,14 +55,44 @@ public class ConnectionAdapter extends BaseAdapter {
 
         Connection connection = (Connection) getItem(position);
 
-        TextView from = (TextView) convertView.findViewById(R.id.from);
-        ConnectionStation fromStation = connection.getFrom();
-        from.setText(fromStation.getStation().getName());
+        TextView departure = (TextView) convertView.findViewById(R.id.departure);
+        TextView arrival = (TextView) convertView.findViewById(R.id.arrival);
+        TextView duration = (TextView) convertView.findViewById(R.id.duration);
+        TextView delay = (TextView) convertView.findViewById(R.id.delay);
 
-        TextView to = (TextView) convertView.findViewById(R.id.to);
-        ConnectionStation toStation = connection.getTo();
-        to.setText(toStation.getStation().getName());
+        try {
+            String departureTime = formateTime(connection.getFrom().getDeparture());
+            departure.setText(departureTime);
+
+            String arrivalTime = formateTime(connection.getTo().getArrival());
+            arrival.setText(arrivalTime);
+
+            //String durationTime = formateTime(connection.getFrom().get());
+            //duration.setText(durationTime);
+
+
+            String delayTime = formateTime(connection.getTo().getDelay());
+            delay.setText(delayTime);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //from.setText();
+
+        //TextView to = (TextView) convertView.findViewById(R.id.to);
+        //ConnectionStation toStation = connection.getTo();
+        //to.setText(toStation.getStation().getName());
 
         return convertView;
+    }
+
+    private String formateTime(String date) throws ParseException {
+        SimpleDateFormat stringtodate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        SimpleDateFormat totime = new SimpleDateFormat("HH:mm");
+
+        Date date1 = stringtodate.parse(date);
+        String time = totime.format(date1);
+
+        return time;
     }
 }
